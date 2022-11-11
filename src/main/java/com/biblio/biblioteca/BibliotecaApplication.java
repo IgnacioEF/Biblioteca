592d7d7c855d5ff37366541d659baad350ecc750
tmantonio <ajtrujillo@indra.es>
2022-11-11 08:58:09 +0100

    ADD resources dedicated to fronted bootstrap template

--- src/main/java/com/biblio/biblioteca/BibliotecaApplication.java
+++ src/main/java/com/biblio/biblioteca/BibliotecaApplication.java
@@ -1,7 +1,13 @@
 package com.biblio.biblioteca;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class BibliotecaApplication {
@@ -9,5 +15,7 @@ public class BibliotecaApplication {
     public static void main(String[] args) {
         SpringApplication.run(BibliotecaApplication.class, args);
     }
+
+
 
 }
